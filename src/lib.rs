c7f7db0aa71bd1e8f04fd90d27d20f3d0a41f5f7
Christopher Rabotin <christopher.rabotin@gmail.com>
2019-01-22 23:28:56 -0700

    Added geoid frame struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
 extern crate nalgebra;
 
-use nalgebra::UnitQuaternion;
+use nalgebra::{UnitQuaternion, Vector3};
 use std::collections::HashMap;
+
+/// Returns the provided angle bounded between 0.0 and 360.0
+pub fn between_0_360(angle: f64) -> f64 {
+    let mut bounded = angle;
+    while bounded > 360.0 {
+        bounded -= 360.0;
+    }
+    while bounded < 0.0 {
+        bounded += 360.0;
+    }
+    bounded
+}
+
+/// Returns the provided angle bounded between -180.0 and +180.0
+pub fn between_pm_180(angle: f64) -> f64 {
+    let mut bounded = angle;
+    while bounded > 180.0 {
+        bounded -= 360.0;
+    }
+    while bounded < -180.0 {
+        bounded += 360.0;
+    }
+    bounded
+}
 
 pub trait Body {
     type FrameType;
@@ -68,6 +92,27 @@ impl Frame for SpaceraftFrame {
     }
 }
 
+pub struct GeoidFrame {
+    parent: Option<Box<GeoidFrame>>,
+    rotation: UnitQuaternion<f64>,
+    body: Box<CelestialFrame>,
+    flattening: f64,
+    semi_major_radius: f64,
+    rotation_rate: f64,
+}
+
+impl Frame for GeoidFrame {
+    fn gm(&self) -> f64 {
+        self.body.gm()
+    }
+    fn parent(self) -> Option<Box<GeoidFrame>> {
+        self.parent
+    }
+    fn rotation_to_parent(&self, at: f64) -> UnitQuaternion<f64> {
+        self.rotation
+    }
+}
+
 #[derive(Copy, Clone, Debug)]
 pub struct State<F>
 where
@@ -117,9 +162,7 @@ where
             frame,
         }
     }
-}
 
-impl State<CelestialFrame> {
     /// Returns the magnitude of the radius vector in km
     pub fn rmag(&self) -> f64 {
         (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
@@ -129,6 +172,9 @@ impl State<CelestialFrame> {
     pub fn vmag(&self) -> f64 {
         (self.vx.powi(2) + self.vy.powi(2) + self.vz.powi(2)).sqrt()
     }
+}
+
+impl State<CelestialFrame> {
     pub fn energy(&self) -> f64 {
         self.vmag().powi(2) / 2.0 - self.gm / self.rmag()
     }
@@ -151,6 +197,119 @@ impl State<SpaceraftFrame> {
             ay: 0.0,
             az: 0.0,
             frame,
+        }
+    }
+}
+
+impl State<GeoidFrame> {
+    /// Creates a new State from the geodetic latitude (φ), longitude (λ) and height with respect to Earth's ellipsoid.
+    ///
+    /// **Units:** degrees, degrees, km
+    /// NOTE: This computation differs from the spherical coordinates because we consider the flattening of Earth.
+    /// Reference: G. Xu and Y. Xu, "GPS", DOI 10.1007/978-3-662-50367-6_2, 2016
+    pub fn from_geodesic(
+        latitude: f64,
+        longitude: f64,
+        height: f64,
+        frame: GeoidFrame,
+    ) -> State<GeoidFrame> {
+        let e2 = 2.0 * frame.flattening - frame.flattening.powi(2);
+        let (sin_long, cos_long) = longitude.to_radians().sin_cos();
+        let (sin_lat, cos_lat) = latitude.to_radians().sin_cos();
+        // page 144
+        let c_earth = frame.semi_major_radius / ((1.0 - e2 * sin_lat.powi(2)).sqrt());
+        let s_earth = (frame.semi_major_radius * (1.0 - frame.flattening).powi(2))
+            / ((1.0 - e2 * sin_lat.powi(2)).sqrt());
+        let ri = (c_earth + height) * cos_lat * cos_long;
+        let rj = (c_earth + height) * cos_lat * sin_long;
+        let rk = (s_earth + height) * sin_lat;
+        let radius = Vector3::new(ri, rj, rk);
+        let velocity = Vector3::new(0.0, 0.0, frame.rotation_rate).cross(&radius);
+        State::<GeoidFrame>::from_position_velocity(
+            radius[(0, 0)],
+            radius[(1, 0)],
+            radius[(2, 0)],
+            velocity[(0, 0)],
+            velocity[(1, 0)],
+            velocity[(2, 0)],
+            frame,
+        )
+    }
+
+    /// Creates a new ECEF state at the provided position.
+    ///
+    /// NOTE: This has the same container as the normal State. Hence, we set the velocity at zero.
+    pub fn from_position(i: f64, j: f64, k: f64, frame: GeoidFrame) -> State<GeoidFrame> {
+        State::<GeoidFrame>::from_position_velocity(i, j, k, 0.0, 0.0, 0.0, frame)
+    }
+
+    /// Returns the I component of this ECEF frame
+    pub fn ri(&self) -> f64 {
+        self.x
+    }
+
+    /// Returns the J component of this ECEF frame
+    pub fn rj(&self) -> f64 {
+        self.y
+    }
+
+    /// Returns the K component of this ECEF frame
+    pub fn rk(&self) -> f64 {
+        self.z
+    }
+
+    /// Returns the geodetic longitude (λ) in degrees. Value is between 0 and 360 degrees.
+    ///
+    /// Although the reference is not Vallado, the math from Vallado proves to be equivalent.
+    /// Reference: G. Xu and Y. Xu, "GPS", DOI 10.1007/978-3-662-50367-6_2, 2016
+    pub fn geodetic_longitude(&self) -> f64 {
+        between_0_360(self.y.atan2(self.x).to_degrees())
+    }
+
+    /// Returns the geodetic latitude (φ) in degrees. Value is between -180 and +180 degrees.
+    ///
+    /// Reference: Vallado, 4th Ed., Algorithm 12 page 172.
+    pub fn geodetic_latitude(&self) -> f64 {
+        let eps = 1e-12;
+        let max_attempts = 20;
+        let mut attempt_no = 0;
+        let r_delta = (self.x.powi(2) + self.y.powi(2)).sqrt();
+        let mut latitude = (self.z / self.rmag()).asin();
+        let e2 = self.frame.flattening * (2.0 - self.frame.flattening);
+        loop {
+            attempt_no += 1;
+            let c_earth =
+                self.frame.semi_major_radius / ((1.0 - e2 * (latitude).sin().powi(2)).sqrt());
+            let new_latitude = (self.z + c_earth * e2 * (latitude).sin()).atan2(r_delta);
+            if (latitude - new_latitude).abs() < eps {
+                return between_pm_180(new_latitude.to_degrees());
+            } else if attempt_no >= max_attempts {
+                println!(
+                    "geodetic latitude failed to converge -- error = {}",
+                    (latitude - new_latitude).abs()
+                );
+                return between_pm_180(new_latitude.to_degrees());
+            }
+            latitude = new_latitude;
+        }
+    }
+
+    /// Returns the geodetic height in km.
+    ///
+    /// Reference: Vallado, 4th Ed., Algorithm 12 page 172.
+    pub fn geodetic_height(&self) -> f64 {
+        let e2 = self.frame.flattening * (2.0 - self.frame.flattening);
+        let latitude = self.geodetic_latitude().to_radians();
+        let sin_lat = latitude.sin();
+        if (latitude - 1.0).abs() < 0.1 {
+            // We are near poles, let's use another formulation.
+            let s_earth = (self.frame.semi_major_radius * (1.0 - self.frame.flattening).powi(2))
+                / ((1.0 - e2 * sin_lat.powi(2)).sqrt());
+            self.z / latitude.sin() - s_earth
+        } else {
+            let c_earth = self.frame.semi_major_radius / ((1.0 - e2 * sin_lat.powi(2)).sqrt());
+            let r_delta = (self.x.powi(2) + self.y.powi(2)).sqrt();
+            r_delta / latitude.cos() - c_earth
         }
     }
 }
